bf204b7e000a69203616479bca28d944b9072c7d
abnmy <10594838+abnmy@users.noreply.github.com>
2023-07-13 11:29:53 +0200

    Update any-product-sum.py

--- python/any-product-sum.py
+++ python/any-product-sum.py
@@ -1,32 +1,30 @@
 """
 Test if a given value can be obtained by adding a number from each list.
-
-The first function is the fastest.
-Code is compatible with python 2 and python 3.
+// Code is compatible with python 2 and python 3.
 """
 from itertools import product
 
 
-def fastest(list_a, list_b, value):
+def f1(list_a, list_b, value):
     return any(a + b == value for a in list_a for b in list_b)
 
 
-def t2(list_a, list_b, value):
+def f2(list_a, list_b, value):
     return any(a + b == value for a, b in product(list_a, list_b))
 
 
-def t3(list_a, list_b, value):
+def f3(list_a, list_b, value):
     return any(s == value for s in map(sum, product(list_a, list_b)))
 
 
 if __name__ == "__main__":
     import timeit
-    NUMBER = 1000000
+    ITERATION = 1000000
 
     FUNCTIONS = (
-        "fastest",
-        "t2",
-        "t3",
+        "f1",
+        "f2",
+        "f3",
     )
 
     # Run
@@ -34,10 +32,10 @@ if __name__ == "__main__":
     for function in FUNCTIONS:
         stmt = "%s([1,2,3,4],[10,20,30,40],42)" % function
         setup = "from __main__ import %s" % function
-        time = timeit.timeit(stmt, setup, number=NUMBER)
+        time = timeit.timeit(stmt, setup, number=ITERATION)
 
-        results.append((funcion, time))
+        results.append((function, time))
 
-    # Display results
-    for function, time in results:
+    # Display
+    for function, time in sorted(results, key=lambda n:n[1], reverse=False):
         print("time %s: %.3f" % (function, time))
