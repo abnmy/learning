468d1d129794bb6f553d63204afe276a373031af
abnmy <10594838+abnmy@users.noreply.github.com>
2023-07-13 18:28:08 +0000

    update pi montecarlo

--- python/multithread_pi_montecarlo.py
+++ python/multithread_pi_montecarlo.py
@@ -8,13 +8,12 @@ def is_in_circle(x: float, y: float, r=1.0) -> bool:
     return sqrt(x**2 + y**2) <= r
 
 
-def montecarlo_pi(n=1_000) -> float:
+def montecarlo_pi(n: int) -> float:
     quarter_pi = sum(is_in_circle(rand0_1f(), rand0_1f()) for _ in range(n))
-    #return quarter_pi * 4 / n
-    return quarter_pi / (n>>2)
+    return quarter_pi / (n>>2)  # quarter_pi * 4 / n
 
 
 
 if __name__ == "__main__":
     print(python_pi)
-    print(montecarlo_pi(1000000))
+    print(montecarlo_pi(10_000_000))
